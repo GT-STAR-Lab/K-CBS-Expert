ae90d2134584e052506ee8e0639f623a872b6795
Reza Torbati <rjtorbati1@gmail.com>
2023-11-14 15:31:46 -0500

    Now runs 4 kcbs threads at once

--- Tasks/generate_logs.sh
+++ Tasks/generate_logs.sh
@@ -2,6 +2,16 @@
 
 #Make sure you've built KCBS before running this!!
 KBS_EXECUTABLE="../build/bicycle"
+CONCURRENT_PROCESSES=4 #Runs this many processes at once
+
+# Function to kill all child processes
+kill_children() {
+  pkill -P $$ # Kills all child processes of the current script
+  exit 1
+}
+
+# Trap SIGINT (Ctrl+C) and call kill_children function
+trap 'kill_children' SIGINT
 
 # Check if the required number of arguments is provided
 if [ $# -ne 5 ]; then
@@ -23,5 +33,10 @@ python3 generate_tasks.py --map $MAP_NAME --agents $NUMBER_OF_AGENTS --name $TAS
 #cd $KBS_DIR
 for ((id = 0; id < NUMBER_OF_TASKS; id++)); do
   TASK_FILE="${TASK_NAME}_${id}"
-    "$KBS_EXECUTABLE" "$(pwd)/$TASK_FILE.xml" "$(pwd)/$TASK_FILE.txt" #"$CWD/$CONFIG"
+  "$KBS_EXECUTABLE" "$(pwd)/$TASK_FILE.xml" "$(pwd)/$TASK_FILE.txt" & #"$CWD/$CONFIG"
+  if (( (id + 1) % $CONCURRENT_PROCESSES == 0 )); then
+    wait # Wait for the previous two processes to finish before starting the next pair
+  fi
 done
+
+wait
